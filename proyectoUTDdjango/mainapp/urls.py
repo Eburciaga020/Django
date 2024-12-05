7af1fbe22695f780ed0ec3a382b1cd38cff25316
Eburciaga020 <148487082+Eburciaga020@users.noreply.github.com>
2024-12-05 13:11:20 -0600

    Add files via upload

---
+++ proyectoUTDdjango/mainapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='inicio'),
+    path('inicio/', views.index, name='inicio'),
+    path('acercade/', views.about, name='acercade'),
+    path('mision/', views.mision, name='mision'),
+    path('vision/', views.vision, name='vision'),
+    path('login/', views.login_user, name='login'),
+    path('registro/', views.register_user, name='registro'),
+    path('logout/', views.logout_user, name='logout'),
+]
+
